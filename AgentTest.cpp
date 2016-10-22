9f111da6c88b0004130431171ca5790a0021a42c
KoyubiStrong <http4645@gmail.com>
2016-10-22 14:12:04 +0900

    Create AgentTest.cpp

---
+++ AgentTest.cpp
@@ -0,0 +1,34 @@
+#include "AgentTest.hpp"
+#include "JointController.hpp"
+#include "MotionSequenceXML.hpp"
+#include "const.hpp"
+
+using namespace futk;
+using namespace Eigen;
+using namespace std;
+
+AgentTest::AgentTest(int argc, char* argv[]) : Agent(argc, argv)
+{
+	motion_ = new MotionSequenceXML("xml/MotionXML/ShakingHead.xml");
+	cycle = 0;
+}
+
+AgentTest::~AgentTest()
+{
+	SAFE_DELETE(motion_)
+}
+
+void AgentTest::Think()
+{
+
+}
+
+void AgentTest::MoveHeadByCode()
+{
+
+}
+
+void AgentTest::MoveHeadByXML()
+{
+
+}
